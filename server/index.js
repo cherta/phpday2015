5754adb8fe405cba38bd382b4a7abba7cafbb41a
Gabriel Chertok <gchertok@gmail.com>
2015-08-05 19:42:17 -0300

    heroku doesnt like 3000 port

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ browserify.settings({
   transform: ['brfs', 'babelify']
 });
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use('/app.js', browserify('client/scripts/main.js'));
 
 app.use(express.static('node_modules'));
@@ -24,7 +26,7 @@ app.get('/talks/:id', function (req, res) {
   res.send(talk);
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
