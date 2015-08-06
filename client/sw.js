a32c2d32055c030c44384dd429b043afe0345557
Gabriel Chertok <gchertok@gmail.com>
2015-08-06 18:53:52 -0300

    solved most of the sw issues

--- client/sw.js
+++ client/sw.js
@@ -1,7 +1,8 @@
 importScripts('scripts/serviceworker-cache-polyfill.js');
 
 var ASSETS_CACHE = 'php-day-2015-agenda';
-var IMAGES_CACHE = 'php-day-2015-agenda-images';
+var GRAVATAR_CACHE = 'php-day-2015-agenda-gravatar';
+var TALKS_CACHE = 'php-day-2105-agenda-talks';
 
 var urlsToCache = [
   '/',
@@ -11,61 +12,74 @@ var urlsToCache = [
   '/images/icons/touch-icon-iphone-retina.png',
   '/images/icons/touch-icon-ipad-retina.png',
   '/images/icons/ms-touch-icon-144x144-precomposed.png',
-  '//fonts.googleapis.com/icon?family=Material+Icons',
-  '//fonts.gstatic.com/s/materialicons/v7/2fcrYFNaTjcS6g4U3t-Y5UEw0lE80llgEseQY3FEmqw.woff2',
-  '//fonts.gstatic.com/s/materialicons/v7/2fcrYFNaTjcS6g4U3t-Y5UEw0lE80llgEseQY3FEmqw.woff2',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v7/2fcrYFNaTjcS6g4U3t-Y5UEw0lE80llgEseQY3FEmqw.woff2',
+  'https://fonts.gstatic.com/s/materialicons/v7/2fcrYFNaTjcS6g4U3t-Y5UEw0lE80llgEseQY3FEmqw.woff2',
   '/styles/app.css',
   '/material-design-lite/material.min.js'
 ];
 
 self.addEventListener('install', function (event) {
-  // Perform install steps
+  // Cache all known urls we will need
   event.waitUntil(
-    caches.open(ASSETS_CACHE)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(ASSETS_CACHE).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// self.onactivate = function(event) {
-//   console.log(event)
-// }
-
 self.addEventListener('fetch', function(event) {
   var requestURL = new URL(event.request.url);
-  if(requestURL.hostname == 'gravatar.com') {
-    event.respondWith(imageURL(event.request));
-  } else {
-    event.respondWith(
-      caches
-        .match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-
-          return fetch(event.request);
-        }
-      )
-    );
+  if(requestURL.hostname === 'gravatar.com') { //Avatars retrieved from gravatar
+    event.respondWith(retrieveFromAndDoCache(event.request, GRAVATAR_CACHE));
+  } else if (requestURL.pathname.indexOf('/talks') === 0) { //Talks json data retrieved from the same host we serve the app
+    event.respondWith(retrieveFromAndDoCache(event.request, TALKS_CACHE));
+  } else { //Everything else
+    event.respondWith(retrieveFromButDoNotCache(event.request));
   }
 });
 
-function imageURL(request) {
-  return caches
-    .match(request)
-    .then(function(response) {
-      if (response) { return response; }
+self.addEventListener('activate', function(event) {
 
-      return fetch(request)
-        .then(function(response) {
-          caches.open(IMAGES_CACHE).then(function(cache) {
-            cache.put(request, response);
-          });
+  var cacheWhitelist = [ASSETS_CACHE, GRAVATAR_CACHE];
 
-      return response.clone();
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+function retrieveFromButDoNotCache(request) {
+  return caches.match(request).then(function(response) {
+    if (response) { return response; }
+    return fetch(request);
+  });
+}
+
+function retrieveFromAndDoCache(request, cacheName) {
+  return caches.match(request).then(function (response) {
+    if (response) { return response; }
+
+    var fetchRequest = request.clone();
+
+    return fetch(fetchRequest).then(function (response) {
+      if(!response || response.status !== 200 || response.type !== 'basic') { return response; }
+
+      var responseToCache = response.clone();
+
+      caches.open(cacheName).then(function (cache) {
+        cache.put(request, responseToCache);
+      });
+
+      return response;
+
     });
   });
 }
